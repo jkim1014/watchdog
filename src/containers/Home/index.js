6635eeea4c03f7bc3343408d10b529eb5abe8d1a
jkim1014 <joonyoungkim@college.harvard.edu>
2019-01-10 14:23:31 -0500

    Added google maps API and auto complete field on home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,72 @@
 import React, { Component } from 'react'
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from 'react-places-autocomplete'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      longitude: '',
+      latitude: '',
+      address: ''
+    }
+  }
+  handleChange = address => {
+    this.setState({ address })
+  }
 
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({latitude: latLng.lat})
+        this.setState({longitude: latLng.lng})
+      })
+      .catch(error => console.error('Error', error))
+  }
   render() {
+    console.log(this.state)
     return (
-      <div>
-        Hello World!
-      </div>
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input'
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item'
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' }
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
     )
   }
 }
